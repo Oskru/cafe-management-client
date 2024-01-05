0473e9a5cb18ddc5bf0bc1ff886c810650585c10
Oskru <oskrupaa@gmail.com>
2024-01-05 07:46:45 +0100

    feat: Add CssBaseline to the global app context #2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
+import { CssBaseline } from '@mui/material';
 
 // Material UI Roboto font
 import '@fontsource/roboto/300.css';
@@ -10,6 +11,7 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
